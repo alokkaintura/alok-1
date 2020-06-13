317465b120bc7cbd5d06be08a2458dc9c36933e0
11kush11410 <66750934+11kush11410@users.noreply.github.com>
2020-06-13 23:39:02 +0530

    commit

---
+++ index.php
@@ -0,0 +1,13 @@
+ <center> WECOME TO MY HYBRID CLOUD WORLD </center> 
+<br />
+<pre>
+<?php
+echo' 
+<html>
+</html>
+';
+echo'
+<center> <h2> i want to hack your privacy..!! </h2> </center>
+';
+?>
+</pre>
